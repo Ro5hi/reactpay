3a6e6ee2091db5778b09c5707e39b30dceae33b2
Ro5hi <slen.detablan@gmail.com>
2021-11-14 19:12:39 -0700

    added useState, added form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import logo from './logo.svg';
+import React, { useState  } from 'react';
 import './App.css';
 
 function App() {
+
+  const [number, setNumber] = useState('');
+  const [name, setName] = useState('');
+  const [expiration, setExpiration] = useState('');
+  const [cvc, setCvc] = useState('');
+  const [save, setSave] = useState('');
+
   return (
     <div className="App">
+      <form>
+        <input type='num' 
+          name='number' 
+          placeholder='Full Card Number' 
+          value={number}
+          onChange={ e => setNumber(e.target.value)}
+          save={e => setSave(e.target.name)}
+        />
+        <input type='text'
+          name='name'
+          placeholder='Name on Card'
+          value={name}
+          onChange={ e => setName(e.target.value)}
+          save={e => setSave(e.target.name)}
+        />
+      </form>
 
     </div>
   );
